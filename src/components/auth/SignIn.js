e6b5698592e376839738dde782faab69eb45d1de
lyndonna.munro@gmail.com <lyndonnamunro@Lyndonnas-Air.verizon.net>
2022-08-04 14:33:27 -0400

    SignUp page Working

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -8,72 +8,58 @@ import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 
 const SignIn = (props) => {
-	// constructor(props) {
-	// 	super(props)
+    // constructor(props) {
+    // 	super(props)
 
-	// 	this.state = {
-	// 		email: '',
-	// 		password: '',
-	// 	}
-	// }
+    // 	this.state = {
+    // 		email: '',
+    // 		password: '',
+    // 	}
+    // }
     const [email, setEmail] = useState('')
-    const [ username, setUsername ] = useState('')
+    const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
 
     const navigate = useNavigate()
 
-	// handleChange = (event) =>
-	// 	this.setState({
-	// 		[event.target.name]: event.target.value,
-	// 	})
+    // handleChange = (event) =>
+    // 	this.setState({
+    // 		[event.target.name]: event.target.value,
+    // 	})
 
-	const onSignIn = (event) => {
-		event.preventDefault()
+    const onSignIn = (event) => {
+        event.preventDefault()
         console.log('the props', props)
-		const { msgAlert, setUser } = props
+        const { msgAlert, setUser } = props
 
-        const credentials = {email, password}
+        const credentials = { email, password }
 
-		signIn(credentials)
-			.then((res) => setUser(res.data.user))
-			.then(() =>
-				msgAlert({
-					heading: 'Sign In Success',
-					message: messages.signInSuccess,
-					variant: 'success',
-				})
-			)
-			.then(() => navigate('/'))
-			.catch((error) => {
+        signIn(credentials)
+            .then((res) => setUser(res.data.user))
+            .then(() =>
+                msgAlert({
+                    heading: 'Sign In Success',
+                    message: messages.signInSuccess,
+                    variant: 'success',
+                })
+            )
+            .then(() => navigate('/'))
+            .catch((error) => {
                 setEmail('')
-                setUsername('')
                 setPassword('')
-				msgAlert({
-					heading: 'Sign In Failed with error: ' + error.message,
-					message: messages.signInFailure,
-					variant: 'danger',
-				})
-			})
-	}
+                msgAlert({
+                    heading: 'Sign In Failed with error: ' + error.message,
+                    message: messages.signInFailure,
+                    variant: 'danger',
+                })
+            })
+    }
 
     return (
         <div className='row'>
             <div className='col-sm-10 col-md-8 mx-auto mt-5'>
                 <h3>Sign In</h3>
                 <Form onSubmit={onSignIn}>
-
-                    <Form.Group controlId='username'>
-                        <Form.Label>Username</Form.Label>
-                        <Form.Control
-                            required
-                            type='username'
-                            name='username'
-                            value={username}
-                            placeholder='Enter username'
-                            onChange={e => setUsername(e.target.value)}
-                        />
-                    </Form.Group>
-
                     <Form.Group controlId='email'>
                         <Form.Label>Email address</Form.Label>
                         <Form.Control
