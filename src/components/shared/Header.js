7119a1494b2815e097d013479d17d0050405c7da
Zene09 <zeneorr@gmail.com>
2022-08-08 08:35:48 -0500

    added fonts

--- src/components/shared/Header.js
+++ src/components/shared/Header.js
@@ -4,7 +4,8 @@ import Navbar from 'react-bootstrap/Navbar'
 import { Link } from 'react-router-dom'
 const linkStyle = {
     color: 'white',
-    textDecoration: 'none'
+    textDecoration: 'none',
+	fontFamily: 'Cinzel, serif'
 }
 const authenticatedOptions = (
 	<>
@@ -52,7 +53,7 @@ const Header = ({ user }) => (
 	<Navbar bg='primary' variant='dark' expand='md'>
 		<Navbar.Brand>
             <Link to='/' style={linkStyle}>
-                MMO & Co Blog Posts
+                MMO & Co Blogs
             </Link>
         </Navbar.Brand>
 		<Navbar.Toggle aria-controls='basic-navbar-nav' />
