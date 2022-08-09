6ee3290163e647640cc17cb536e869b0cca35182
Zene09 <zeneorr@gmail.com>
2022-08-08 19:33:16 -0500

    added styling to auth pages

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -7,6 +7,13 @@ import messages from '../shared/AutoDismissAlert/messages'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 
+const rowStyles = {
+    fontFamily: 'Marcellus, serif',
+    textShadow: '5px',
+    backgroundColor: '#0A2733',
+    color: '#F4A460'
+}
+
 const SignIn = (props) => {
     // constructor(props) {
     // 	super(props)
@@ -55,12 +62,13 @@ const SignIn = (props) => {
     }
 
     return (
-        <div className='row'>
+        <div className='row'
+                style={ rowStyles }>
             <div className='col-sm-10 col-md-8 mx-auto mt-5'>
-                <h3>Sign In</h3>
+                <h3 style={{ marginBottom: '15px' }}>Sign In</h3>
                 <Form onSubmit={onSignIn}>
                     <Form.Group controlId='email'>
-                        <Form.Label>Email address</Form.Label>
+                        <Form.Label style={{ marginBottom: '5px' }}>Email address</Form.Label>
                         <Form.Control
                             required
                             type='email'
@@ -68,10 +76,11 @@ const SignIn = (props) => {
                             value={email}
                             placeholder='Enter email'
                             onChange={e => setEmail(e.target.value)}
+                            style={{ marginBottom: '10px' }}
                         />
                     </Form.Group>
                     <Form.Group controlId='password'>
-                        <Form.Label>Password</Form.Label>
+                        <Form.Label style={{ marginBottom: '5px' }}>Password</Form.Label>
                         <Form.Control
                             required
                             name='password'
@@ -79,9 +88,18 @@ const SignIn = (props) => {
                             type='password'
                             placeholder='Password'
                             onChange={e => setPassword(e.target.value)}
+                            style={{ marginBottom: '10px' }}
                         />
                     </Form.Group>
-                    <Button variant='outline-primary' type='submit'>
+                    <Button 
+                        variant='outline-light' 
+                        type='submit'
+                        style={{ 
+                            fontFamily: 'Marcellus, serif',
+                            backgroundColor: '#003309',
+                            borderColor: '#FEF4EC',
+                            margin: '10px'
+                        }}>
                         Submit
                     </Button>
                 </Form>
--- src/components/auth/SignOut.js
+++ src/components/auth/SignOut.js
@@ -5,6 +5,12 @@ import {Button, ButtonGroup} from 'react-bootstrap'
 import { signOut } from '../../api/auth'
 import messages from '../shared/AutoDismissAlert/messages'
 
+const rowStyles = {
+        fontFamily: 'Marcellus, serif',
+        textShadow: '5px',
+        backgroundColor: '#0A2733',
+        color: '#F4A460'
+}
 const SignOut = (props) => {
 	const { msgAlert, clearUser, user } = props
     console.log(props)
@@ -30,15 +36,30 @@ const SignOut = (props) => {
 
 	return (
 		<>
-            <div className='row'>
+            <div className='row' 
+                style={ rowStyles }>
                 <div className='col-sm-10 col-md-8 mx-auto mt-5'>
                     <h2>Are you sure you want to sign out?</h2>
                     <small>We hate to see you go...</small><br/>
                     <ButtonGroup>
-                        <Button variant='danger' onClick={onSignOut}>
+                        <Button 
+                        variant='outline-light' 
+                        onClick={onSignOut}
+                        style={{ 
+                            backgroundColor: '#B22206',
+                            borderColor: '#B22206',
+                            margin: '10px'
+                        }}>
                             Sign Out
                         </Button>
-                        <Button variant='outline-primary' onClick={onCancel}>
+                        <Button 
+                        variant='outline-dark' 
+                        onClick={onCancel}
+                        style={{ 
+                            backgroundColor: '#FEF4EC',
+                            borderColor: '#FEF4EC',
+                            margin: '10px'
+                        }}>
                             Cancel
                         </Button>
                     </ButtonGroup>
